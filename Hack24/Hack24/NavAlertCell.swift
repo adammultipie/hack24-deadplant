1495daaa8ff63a4b633cc53bc4f9bfa3ce0e6ccd
Adam Mitchell <adam@multipie.co.uk>
2016-03-20 07:51:51 +0000

    Ain't nobody got time for commit messages

---
+++ Hack24/Hack24/NavAlertCell.swift
@@ -0,0 +1,24 @@
+//
+//  NavAlertCell.swift
+//  Hack24
+//
+//  Created by Adam Mitchell on 20/03/2016.
+//  Copyright © 2016 MultiPie. All rights reserved.
+//
+
+import UIKit
+
+class NavAlertCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
